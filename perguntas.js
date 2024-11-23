f44ff08d3a005c2f31fb2986a39ab2619418c723
Caua90azul <ocaua191@gmail.com>
2024-11-22 21:35:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'futebol' ,   
+    'O Brasil perdeu de quanto para a Alemanha?',
+    'De 7x1'
+)
+
+criaCartao(
+    'Carrros',
+    'Qual é o carro mais rápido do mundo?',
+    'Hennessey Venon F5 Revolution Roadster'
+)
+
+criaCartao(
+    'filmes',
+    'Quem criou Harry Potter?',
+    'J.K.Rowling',
+)
